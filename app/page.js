2218e4f82b0e02f123d1779d0e0678ca10b24d84
Dilan Kaplan <45143995+dilankaplan@users.noreply.github.com>
2023-07-28 23:14:09 +0300

    base

--- app/page.js
+++ app/page.js
@@ -1,17 +1,40 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Footer, Navbar } from "../components";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
 
 const Page = () => (
-  <div className='bg-primary-black overflow-hidden'>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
-    <World />
-    <Insights />
-    <Feedback />
+    <div className="realtive">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="realtive">
+      <GetStarted />
+      <div className="gradient-03 z-0" />
+      <WhatsNew />
+    </div>
+    <div className="realtive">
+      <World />
+      <div className="gradient-03 z-0" />
+      <Insights />
+    </div>
+    <div className="realtive">
+      <Insights />
+      <div className="gradient-03 z-0" />
+
+      <Feedback />
+    </div>
     <Footer />
   </div>
 );
